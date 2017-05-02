6414272f9108e0a154c1932911895bf6d44caae7
li xing <onsoul@qq.com>
2017-05-02 16:26:24 +0800

    修正消费者

--- os-c/src/main/java/com/wise/process/ChatApplication.java
+++ os-c/src/main/java/com/wise/process/ChatApplication.java
@@ -6,8 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 import com.wise.comet.ChatServer;
 
@@ -15,8 +19,9 @@ import com.wise.comet.ChatServer;
  * 程序入口
  */
 @SpringBootApplication
-@ComponentScan("com.hitler.chat")
-// @ImportResource("classpath:dubbo-consumer.xml")    //dubbo时启用
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@ComponentScan(value={"com.wise.comet","com.wise.web"})
+@ImportResource("classpath:dubbo-consumer.xml")    //dubbo时启用
 public class ChatApplication implements CommandLineRunner {
 
     private static Logger LOG = LoggerFactory.getLogger(ChatApplication.class);
